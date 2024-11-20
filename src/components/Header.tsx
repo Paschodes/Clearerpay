8478fe245ba3a2089ff1b168ba2b8b3d3bab5372
Paschodes <paschal.c.chikwendu@gmail.com>
2024-11-20 23:36:31 +0100

    initial commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import { TiUserOutline } from "react-icons/ti";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { RiSettings4Line } from "react-icons/ri";
 
-const Header = ({ logo, userName }: HeaderProps) => (
+const Header = ({ logo }: HeaderProps) => (
   <header className="flex items-center justify-between p-4 pb-0 bg-gray-50 shadow-md border-b border-gray-200 w-full flex-row">
     <div className="flex items-center">
       <img src={logo} alt="Clearerpay Logo" className="w-32" />
